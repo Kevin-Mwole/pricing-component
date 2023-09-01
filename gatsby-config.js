205e6fb8aa31d353da2a742b4d79f03c13133f3c
Kevin Mwole <mwole100@gmail.com>
2023-09-01 23:23:31 +0300

    Correct social image url

---
+++ gatsby-config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  siteMetadata: {
+    title: `Better Storage Solutions`,
+    description: `Select your storage plan option`,
+    author: `@alexkmarshall1`,
+    canonicalUrl: `https://pensive-wilson-d8ed50.netlify.app`,
+  },
+  plugins: [
+    `gatsby-plugin-postcss`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `pricing component`,
+        short_name: `pricing`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/favicon-32x32.png`, // This path is relative to the root of the site.
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+};
